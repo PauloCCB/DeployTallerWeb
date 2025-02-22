4c51f6c37345660611a218da84ddd3b0a2a87caa
PauloCCB <paulo.napasa3@gmail.com>
2025-02-22 01:37:08 -0500

    Se hace modificaciones del next.config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+
+  output: "standalone",
+  eslint: {
+    ignoreDuringBuilds: true, // Ignorar errores de ESLint en la construcción
+  },
+  typescript: {
+    ignoreBuildErrors: true, // Ignorar errores de TypeScript en la construcción
+  },
 };
 
 export default nextConfig;
